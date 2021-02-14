6018a1fc1da31e46c95b7e8275d231e3962b6484
David Fonseca <47060767+dgfonseca@users.noreply.github.com>
2021-02-14 15:57:42 -0500

    Agregado
    Agregado

--- apropiacion_tecnologica/settings.py
+++ apropiacion_tecnologica/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
- SECURITY WARNING: keep the secret key used in production secret!
+#SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
